8d3e7c86e2c4e6cf6fe5338cc5518334c1a448da
wjs <w1014221775@126.com>
2019-11-20 17:45:26 +0800

    add model problem

--- online_judge/oj/models.py
+++ online_judge/oj/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    prob_id = models.IntegerField(default=0)
+    title = models.CharField(max_length=30)
+    time_lim = models.IntegerField()
+    mem_lim = models.IntegerField()
+    description = models.TextField()
+    input_format = models.TextField()
+    output_format = models.TextField()
+    example_inp = models.TextField()
+    exmaple_outp = models.TextField()
+    data_input = models.FileField()
+    data_output = models.FileField()
+
+    def add_problem(self):
+        self.save()
--- online_judge/online_judge/settings.py
+++ online_judge/online_judge/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oj',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROBLEM_DATA_ROOT = os.path.join(BASE_DIR, 'static/problem_data')
