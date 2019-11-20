82b756b77f0bb0d6f5cca446c3130a2aa3b369c2
liujianghu250 <33927770+liujianghu250@users.noreply.github.com>
2019-11-20 23:51:44 +0800

    Add files via upload

--- online_judge/oj/views.py
+++ online_judge/oj/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Problem
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html', {'Problem': problem})
+def problem_list(request,page):
+    PAGENUM = 1
+    start = (page-1) * PAGENUM
+    stop = page * PAGENUM
+
+    myslice = slice(start,stop,1)
+    problems = Problem.objects.order_by('prob_id')
+    npage = (len(problems)-1) // PAGENUM + 1
+    pages = []
+    for i in range(1,npage+1):
+        pages.append(i)
+
+    problems = problems[myslice]
+
+    return render(request, 'problem_list.html', {'problems': problems,'page' : page,'pages' : pages})
+
+def prob_detail(request,prob_id):
+    problem = Problem.objects.get(prob_id = prob_id)
+    return render(request, 'prob_detail.html', {'problem': problem})
+
+def submit(request,prob_id):
+    #判断是否登录,若登录则跳到submit页面,否则登录页面
+    return render(request,'submit.html',{'prob_id': prob_id})
